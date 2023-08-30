0dd7542869cd70c6527900ff46ee8a4b15bca027
Ivan Istomin <139970052+vanyushais@users.noreply.github.com>
2023-08-30 15:54:49 +0300

    Add files via upload

---
+++ H4CK3R News/H4CK3R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4CK3R_NewsApp.swift
+//  H4CK3R News
+//
+//  Created by Ivan Istomin on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4CK3R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4CK3R News/Models/PostData.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
